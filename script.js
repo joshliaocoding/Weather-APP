9e06c1f4067344aba30fe14a12c8f30ea8ff5601
joshliaocoding <joshliao0607@gmail.com>
2025-03-28 11:20:10 +0800

    Finish the structure of HTML and the styles.css.

---
+++ script.js
@@ -0,0 +1,13 @@
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "apikey";
+
+weatherForm.addEventListener("submit", event => {
+console.log("You click the button");
+});
+
+async function getWeatherData(city){
+
+}
+
